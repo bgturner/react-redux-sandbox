6047294a6fac950e1f05a749fcc08f3726d28217
Benjamin Turner <benjamin@passionsplay.com>
2016-03-20 13:46:36 -0700

    Some incorrect coding styles to fail TravisCI

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -5,6 +5,7 @@ export class HomeView extends React.Component {
   render () {
     return (
       <div>
+				<div>Incorrect coding style to fail TravisCI</div>
         <h1>Your New Project</h1>
       </div>
     )
