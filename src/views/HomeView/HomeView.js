241fcd4320070fe89c9ae44e6a88765bfcfce1d9
Benjamin Turner <benjamin@passionsplay.com>
2016-03-20 14:04:42 -0700

    A commit that changes dom and passes travis

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -5,6 +5,7 @@ export class HomeView extends React.Component {
   render () {
     return (
       <div>
+        <div>Simple dom change to pass TravisCI</div>
         <h1>Your New Project</h1>
       </div>
     )
